7840c0e577504346cbe8ffc6157bdbb544fa1b06
김수환 <suhwan@kakao.com>
2024-05-17 17:46:58 +0900

    [level 1] Title: 행렬의 덧셈, Time: 23.22 ms, Memory: 22.9 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -0,0 +1,12 @@
+def solution(arr1, arr2):
+    answer = []
+
+    for i, j in zip(arr1, arr2):
+        l = []
+
+        for x, y in zip(i, j):
+            l.append(x + y)
+
+        answer.append(l)
+        
+    return answer
