ca1303275959c7cbb1826172235734d8c7731af2
Suhwan <suhwan@kakao.com>
2024-06-12 01:03:30 +0900

    [Gold V] Title: 암호 만들기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,33 @@
+l, c = map(int, input().split())
+arr = list(map(str, input().split()))
+arr.sort()
+
+choose = []
+check = ['a', 'e', 'i', 'o', 'u']
+check2 = ['b', 'c', 'd', 'f', 'g', 
+          'h', 'j', 'k', 'l', 'n', 
+          'm', 'p', 'q', 'r', 's', 
+          't', 'v', 'w', 'x', 'y', 'z']
+
+def combination(index, level):
+  if level == l:
+    cnt, cnt2 = 0, 0
+    for ch in choose:
+      if ch in check:
+        cnt += 1
+      if ch in check2:
+        cnt2 += 1
+
+    if cnt >= 1 and cnt2 >= 2:
+      for ch in choose:
+        print(ch, end='')
+      print()
+
+    return
+  
+  for i in range(index, c):
+    choose.append(arr[i])
+    combination(i + 1, level + 1)
+    choose.pop()
+
+combination(0, 0)
