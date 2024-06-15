cfaafd88a31baf0299b465831ec2506af411ee29
Suhwan <suhwan@kakao.com>
2024-06-15 17:45:33 +0900

    [Silver III] Title: 숫자 야구, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ Python/백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,44 +1,30 @@
+from itertools import permutations
+
 n = int(input())
 
-numbers = [(list(map(str, input().split()))) for _ in range(n)]
+t = []
+for _ in range(n):
+  t.append(list(map(str, input().split())))
+
 answer = 0
 
-for a in range(1, 10):
-    for b in range(1, 10):
-        for c in range(1, 10):
-            counter = 0
+for per in permutations(range(1, 10), 3):
+  is_ok = True
 
-            if a == b or b == c or c == a:
-                continue
+  for score, strike, ball in t:
+    real_strike = real_ball = 0
 
-            for arr in numbers:
-                number = list(arr[0])
-
-                strike_count = 0
-                ball_count = 0
-
-                if a == int(number[0]):
-                    strike_count += 1
-                if b == int(number[1]):
-                    strike_count += 1
-                if c == int(number[2]):
-                    strike_count += 1
-
-                if a == int(number[1]) or a == int(number[2]):
-                    ball_count += 1
-                if b == int(number[0]) or b == int(number[2]):
-                    ball_count += 1
-                if c == int(number[0]) or c == int(number[1]):
-                    ball_count += 1
-
-                if strike_count != int(arr[1]):
-                    break
-                if ball_count != int(arr[2]):
-                    break
-
-                counter += 1
-
-            if counter == n:
-                answer += 1
+    for i in range(3):
+      if str(per[i]) == score[i]:
+        real_strike += 1
+      elif str(per[i]) in score:
+        real_ball += 1
+    
+    if int(strike) != real_strike or int(ball) != real_ball:
+      is_ok = False
+      break
+  
+  if is_ok:
+    answer += 1
 
 print(answer)
