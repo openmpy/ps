b6852bef52428e0dcf43ca7fdf8fd4fd0272035a
Suhwan <suhwan@kakao.com>
2024-06-18 12:51:25 +0900

    [Silver II] Title: 이동하기, Time: 856 ms, Memory: 78784 KB -BaekjoonHub

---
+++ Python/백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+
+arr = [[0] + list(map(int, input().split())) for _ in range(N)]
+arr = [[0] * (M + 1)] + arr
+
+dp = [[0 for _ in range(M + 1)] for _ in range(N + 1)]
+dp[1][1] = arr[1][1]
+
+for i in range(1, N + 1):
+  for j in range(1, M + 1):
+    dp[i][j] = arr[i][j]
+    dp[i][j] += max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+
+print(dp[N][M])
